468fa2de38f3b8d4e7ac31d6982767deba75ba39
Behl <aayush.behl@ubc.ca>
2022-08-08 13:24:58 -0700

    Task and ECR Repository creation added to CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,23 +1,50 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import * as cdk from 'aws-cdk-lib'
+import * as cdk from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
-import * as iam from 'aws-cdk-lib/aws-iam' 
-import * as s3 from 'aws-cdk-lib/aws-s3'
+//import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam'; 
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     // The code that defines your stack goes here
-    const repo = new ecr.Repository(this,'testrepository', {
+    
+    const repo = new ecr.Repository(this,"repository",{
       encryption: ecr.RepositoryEncryption.KMS
     });
 
     const bucket = new s3.Bucket(this,"testS3",{
-      autoDeleteObjects: true,
-      bucketName: "TestBucket"
+      bucketName: "integrals-bucket"
     });
+
+    const ecsTaskRole = new iam.Role(this, 'ecsTaskRole',{
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      description: 'Role for ECS Tasks',
+      managedPolicies: [
+        ManagedPolicy.fromManagedPolicyArn(this,'ecsPolicy','arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy')
+      ]
+    });
+
+    const ecsTask = new ecs.TaskDefinition(this,'ecsTask',{
+      compatibility: ecs.Compatibility.FARGATE,
+      executionRole: ecsTaskRole,
+      cpu: '1024',
+      memoryMiB: '2048',
+      runtimePlatform: {
+        operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
+      }
+    });
+
+
+    ecsTask.addContainer('container',{
+      image: ecs.ContainerImage.fromEcrRepository(repo,'latest'),  
+      containerName: 'integralsExecution'
+    })
 
     /*
 
@@ -62,7 +89,7 @@ export class CdkStack extends Stack {
 
     new cdk.CfnOutput(this,'repoName',{
       value: repo.repositoryUri,
-      description: 'Repository URI'
+      description: 'Repository Name'
     })
   }
 }
