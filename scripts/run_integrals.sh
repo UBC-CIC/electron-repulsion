8fee8e491e4d61821e3f37f69d7360a5af1237a5
Behl <aayush.behl@ubc.ca>
2022-09-21 14:39:27 -0700

    Changed shell script to account for the max_instances_bounding version

--- scripts/run_integrals.sh
+++ scripts/run_integrals.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-# Both of these must be set for a batch job
-if [ ! -z $ARGS_PATH ] && [ ! -z $AWS_BATCH_JOB_ARRAY_INDEX ]
+# If AWS_BATCH_JOB_INDEX is set, it means execution is an array batch execution
+if [ ! -z $AWS_BATCH_JOB_ARRAY_INDEX ]
 then
     aws s3 cp $ARGS_PATH/batch_args.txt /args.txt
-    LINE=$((AWS_BATCH_JOB_ARRAY_INDEX + 1))
+    ADJ_LINE=$((AWS_BATCH_JOB_ARRAY_INDEX + $LINE))
+    COMMAND=$(sed -n ${ADJ_LINE}p /args.txt)
+    ./../integrals/integrals $@ $COMMAND | tee output.json
+    TO_REPLACE="#JOB_NUMBER"
+    REPLACEMENT="_${ADJ_LINE-1}"
+    NEW_JSON_PATH="${JSON_OUTPUT_PATH/"$TO_REPLACE"/"$REPLACEMENT"}"
+    echo $NEW_JSON_PATH
+    aws s3 cp output.json $NEW_JSON_PATH
+elif [ ! -z $LINE ] # If LINE is set but not AWS_BATCH_JOB_INDEX, we are executing a single remaining execution on ECS
+then
+    aws s3 cp $ARGS_PATH/batch_args.txt /args.txt
     COMMAND=$(sed -n ${LINE}p /args.txt)
     ./../integrals/integrals $@ $COMMAND | tee output.json
     TO_REPLACE="#JOB_NUMBER"
-    REPLACEMENT="_${AWS_BATCH_JOB_ARRAY_INDEX}"
+    REPLACEMENT="_${LINE-1}"
     NEW_JSON_PATH="${JSON_OUTPUT_PATH/"$TO_REPLACE"/"$REPLACEMENT"}"
     echo $NEW_JSON_PATH
     aws s3 cp output.json $NEW_JSON_PATH
