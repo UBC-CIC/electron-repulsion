41995120ccf568b8aaa3ef4f240f95024375bcd1
arshiamoghimi <arshiamoghimi98@gmail.com>
2023-06-13 19:42:01 +0000

    updated deleteJob lambda

--- cdk/lambda/deleteJob/lambda_function.py
+++ cdk/lambda/deleteJob/lambda_function.py
@@ -1,46 +1,34 @@
 import json
 import boto3
 import os
-from urllib.parse import urlparse
 
-dynamo = boto3.client('dynamodb')
+dynamo = boto3.client("dynamodb")
 
-deleted_job_table = os.environ['DELETED_JOB_TABLE']
-batch_table = os.environ['BATCH_TABLE']
+deleted_job_table = os.environ["DELETED_JOB_TABLE"]
+batch_table = os.environ["BATCH_TABLE"]
+
+
+def verify_inputs(event):
+    if "jobid" not in event:
+        raise Exception("jobid is required")
 
 
 def lambda_handler(event, context):
-    jobid = event['jobid']
-    try:
-        dynamo.put_item(
-            TableName=deleted_job_table,
-            Item={
-                'jobid': {'S': jobid},
-            }
-        )
-        dynamo.delete_item(
-            TableName=batch_table,
-            Key={
-                'jobid': {'S': jobid}
-            }
-        )
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({
+    verify_inputs(event)
+    jobid = event["jobid"]
+    dynamo.put_item(
+        TableName=deleted_job_table,
+        Item={
+            "jobid": {"S": jobid},
+        },
+    )
+    dynamo.delete_item(TableName=batch_table, Key={"jobid": {"S": jobid}})
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(
+            {
                 "jobid ": jobid,
-            })
-        }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({
-                "jobid ": jobid,
-                "error": str(e)
-            })
-        }
+            }
+        ),
+    }
